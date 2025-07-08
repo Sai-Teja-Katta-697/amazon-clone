ce204ca7207445755b1be813cf887f5b9f7435eb
Sai Teja <saitejakatta697@gmail.com>
2025-07-08 06:52:35 +0530

    Made cart symbol show updated vaue

--- amazon-javascript/amazon.js
+++ amazon-javascript/amazon.js
@@ -76,10 +76,10 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
         quantity: 1,
       });
     }
-    console.log(
-      cart.forEach((item) => {
-        console.log(item);
-      })
-    );
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+    document.querySelector(".js-cart-quantity").textContent = cartQuantity;
   });
 });
